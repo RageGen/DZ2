1d9e01df40c1f3ccdfa049812973ab46166b02fc
RageGen <zamorin.daniil@mail.ru>
2022-05-19 17:51:02 +0300

    .

--- Vector_Second/Vector.cpp
+++ Vector_Second/Vector.cpp
@@ -1,8 +1,83 @@
 #include <vector>
 #include "Student.hpp"
+#include <Windows.h>
 using namespace std;
 vector<Student>StudentList;
 vector<Student>StudentList2;
+vector<Student>StudentList3;
+void sort(vector<Student>&Storage)
+{
+	cout << "1. По убыванию\n";
+	cout << "2. По возрастанию\n>>> ";
+	int check = 0;
+	cin >> check;
+	switch (check)
+	{
+	case 1:
+		for (int i = 0; i < Storage.size(); i++)
+		{
+			for (int j = i + 1; j < Storage.size(); j++)
+			{
+				if (Storage[i] > Storage[j])
+				{
+					Student temp = Storage[i];
+					Storage[i] = Storage[j];
+					Storage[j] = temp;
+				}
+			}
+		}
+	case 2:
+		for (int i = 0; i < Storage.size(); i++)
+		{
+			for (int j = i + 1; j < Storage.size(); j++)
+			{
+				if (Storage[i] < Storage[j])
+				{
+					Student temp = Storage[i];
+					Storage[i] = Storage[j];
+					Storage[j] = temp;
+				}
+			}
+		}
+	}
+}
+void SearchAndSwap()
+{
+	string SurnmaeToSearch;
+	cout << "Фамилия\n>>> ";
+	cin >> SurnmaeToSearch;
+	for (int i = 0; i < StudentList.size(); i++)
+	{
+		if (StudentList[i].m_GetSurname() == SurnmaeToSearch)
+		{
+			StudentList2.push_back(StudentList[i]);
+		}
+	}
+
+}
+void SearchAndCount()
+{
+	string SurnmaeToSearch;
+	int counter = 0;
+	cout << "Фамилия\n>>> ";
+	cin >> SurnmaeToSearch;
+	for (int i = 0; i < StudentList3.size(); i++)
+	{
+		if (StudentList3[i].m_GetSurname() == SurnmaeToSearch)
+		{
+			counter += 1;
+		}
+	}
+	if (counter > 0)
+	{
+		cout << "Есть- " << counter;
+	}
+	else
+	{
+		cout << "Нету" << endl;
+	}
+}
+
 void LocalInformation()
 {
 	StudentList.resize(3);
@@ -59,43 +134,48 @@ void LocalInformation2()
 	StudentList2[2].m_SetPassword("password12345");
 
 }
-void Erase(vector<Student>& storage, int n,int first)
-{
-	for (int index = first; index <= n; ++index)
-	{
-		storage.erase(storage.begin() + first);
-	}
-}
 int main()
 {
-	setlocale(LC_ALL, "rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	LocalInformation();
-	LocalInformation2();
-	cout << "Вывод вектора" << endl;
+	for (auto i = StudentList.begin(); i < StudentList.end(); i++)
+	{
+		cout << *i << "\n";
+	}
+	cout << "\n";
+	sort(StudentList);
+	for (auto i = StudentList.begin(); i < StudentList.end(); i++)
+	{
+		cout << *i << "\n";
+	}
+	cout << "\n";
+	SearchAndSwap();
+	cout << "\n";
+	cout << "Второй контейнер" << endl;
+	for (auto i = StudentList2.begin(); i < StudentList2.end(); i++)
+	{
+		cout << *i << "\n";
+	}
+	cout << "\n";
+	cout << "Сортировка первого контейнера" << endl;
+	sort(StudentList);
+	cout << "\n";
+	cout << "Сортировка второго контейнера" << endl;
+	sort(StudentList2);
+	cout << "\n";
+	cout << "Третий контейнер" << endl;
 	for (int i = 0; i < StudentList.size(); i++)
 	{
-		cout << StudentList[i]<<"\n";
+		StudentList3.push_back(StudentList[i]);
 	}
-	StudentList.pop_back();
-	StudentList[0].m_SetAge(30);
-	cout << "Вывод измененого вектора" << endl;
-	for (auto i = StudentList.begin(); i < StudentList.end(); i++)
+	for (int i = 0; i < StudentList2.size(); i++)
+	{
+		StudentList3.push_back(StudentList2[i]);
+	}
+	for (auto i = StudentList3.begin(); i < StudentList3.end(); i++)
 	{
 		cout << *i << "\n";
 	}
-	cout << "Первый контейнер" << endl;
-	Erase(StudentList, 1, 1);
-	for (auto i = StudentList.begin(); i < StudentList.end(); i++)
-	{
-		cout << *i << "\n";
-	}
-	cout << "Первый+второй контейнер" << endl;
-	for (auto i = 0; i < StudentList2.size(); i++)
-	{
-		StudentList.push_back(StudentList2[i]);
-	}
-	for (auto i = StudentList.begin(); i < StudentList.end(); i++)
-	{
-		cout << *i << "\n";
-	}
+	SearchAndCount();
 }
